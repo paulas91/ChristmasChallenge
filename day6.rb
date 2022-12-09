88b9ad4b9e6c442a909d833b4eddf53e94b7e6c6
paulas91 <paulinaskopp@gmail.com>
2022-12-09 06:34:50 +0100

    Christmas day6

--- day6.rb
+++ day6.rb
@@ -1,87 +1,32 @@
-* Mikolaj postanowil przygotować w tym roku potrawy swiateczne samodzielnie.
-     * Dostał od mamy listę przepisów, dzieki którym stworzy pyszne, tradycyjne potrawy!
-     * Ale najpierw musi pójść na zakupy. Pomoż Mikołajowi i wygeneruj listę zakupów z listy przepisów.
-     * Dla ułatwienia załóż, że wszystkie produkty są w tych samych jednostkach, wiec po prostu podlicz ilości.
-     * P.S. Nie polecam używać tych przepisów, zostały one maksymalnie uproszone do celu zadania. :D
+# * Mikolaj postanowil przygotować w tym roku potrawy swiateczne samodzielnie.
+#      * Dostał od mamy listę przepisów, dzieki którym stworzy pyszne, tradycyjne potrawy!
+#      * Ale najpierw musi pójść na zakupy. Pomoż Mikołajowi i wygeneruj listę zakupów z listy przepisów.
+#      * Dla ułatwienia załóż, że wszystkie produkty są w tych samych jednostkach, wiec po prostu podlicz ilości.
+#      * P.S. Nie polecam używać tych przepisów, zostały one maksymalnie uproszone do celu zadania. :D
 
 
-      List.of(
-        new Recipe(
-                "Murzynek",
-                Map.of(
-                        "Maka", 300,
-                        "Cukier", 100,
-                        "Jajka", 3,
-                        "Czekolada", 200,
-                        "Maslo", 200
-                ),
-                "Piekarnik nagrzać do temp. 160 stopni [...]"),
-        new Recipe(
-                "Piernik",
-                Map.of(
-                        "Maka", 500,
-                        "Cukier", 180,
-                        "Miod", 250,
-                        "Jajka", 1,
-                        "Mleko", 80,
-                        "Maslo", 150
-                ),
-                "Piekarnik nagrzać do temp. 180 stopni [...]"),
-        new Recipe(
-                "Sernik",
-                Map.of(
-                        "Maka", 100,
-                        "Cukier", 200,
-                        "Jajka", 6,
-                        "Ser", 1000,
-                        "Maslo", 200
-                ),
-                "Piekarnik nagrzać do temp. 160 stopni [...]"),
-        new Recipe(
-                "Uszka",
-                Map.of(
-                        "Maka", 300,
-                        "Woda", 100,
-                        "Jajka", 1,
-                        "Sol", 1,
-                        "Maslo", 200,
-                        "Grzyby", 150,
-                        "Cebula", 1
-                ),
-                "Zarobić ciasto z mąki, wody, soli i jajka [...]"),
-        new Recipe(
-                "Barszcz",
-                Map.of(
-                        "Buraki", 1000,
-                        "Bulion", 1500,
-                        "Zakwas", 200,
-                        "Sol", 1
-                ),
-                "Buraki rozdrobnić i zagotować z bulionem [...]"),
-        new Recipe(
-                "Pierogi",
-                Map.of(
-                        "Maka", 300,
-                        "Woda", 100,
-                        "Jajka", 1,
-                        "Kapusta", 500,
-                        "Grzyby", 50,
-                        "Cebula", 1
-                ),
-                "Zarobić ciasto z mąki, wody, soli i jajka [...]"),
-        new Recipe(
-                "Karp",
-                Map.of(
-                        "Karp", 1000,
-                        "Maka", 50,
-                        "Mleko", 500,
-                        "Sol", 1,
-                        "Olej", 500
-                ),
-                "Rybę umyć i pokroić w dzwonki [...]")
-);
-}
+     class Recipe
+       attr_reader :ingrediens, :name
+
+        def initialize(name, ingrediens, description)
+          @ingrediens = ingrediens
+        end
+     end
 
 
+      list = [
+        Recipe.new("Murzynek", {"Maka" => 300, "Cukier" => 100, "Jajka" => 3, "Czekolada" => 200, "Masło" => 200 }, "Piekarnik nagrzać do temp. 160 stopni [...]"),
+        Recipe.new("Piernik", {"Maka" => 500, "Cukier" => 180, "Miód" => 250, "Jajka" => 1, "Masło" => 150, "Mleko" => 80 }, "Piekarnik nagrzać do temp. 180 stopni [...]"),
+        Recipe.new("Sernik", {"Maka" => 100, "Cukier" => 200, "Jajka" => 6, "Ser" => 1000, "Masło" => 200 }, "Piekarnik nagrzać do temp. 160 stopni [...]"),
+        Recipe.new("Uszka", {"Maka" => 300, "Woda" => 100, "Jajka" => 1, "Sól" => 1, "Masło" => 200, "Grzyby" => 150, "Cebula" => 1 }, "Zarobić ciasto z mąki, wody, soli i jajka [...]"),
+        Recipe.new("Barszcz", {"Buraki" => 1000, "Bulion" => 1500, "Zakwas" => 200, "Sól" => 1 }, "Buraki rozdrobnić i zagotować z bulionem [...]"),
+        Recipe.new("Pierogi", {"Maka" => 300, "Woda" => 100, "Jajka" => 1, "Kapusta" => 500, "Grzyby" => 50, "Cebula" => 1 }, "Zarobić ciasto z mąki, wody, soli i jajka [...]"),
+        Recipe.new("Karp", {"Karp" => 1000, "Maka" => 50, "Mleko" => 500, "Sól" => 1, "Olej" => 500 }, "Rybę umyć i pokroić w dzwonki [...]")
+      ]
 
+      hash = {}
+      list.each do |element|
+        element.ingrediens.each { |key, value| hash.has_key?(key) ? hash[key] += value : hash[key] = value }
+      end
 
+      hash.each { |key, value| puts "#{key}: #{value}" }
